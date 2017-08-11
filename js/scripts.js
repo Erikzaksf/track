f11d242c639ec00da266f2ee2ba148cccf2a6983
Erik Zakrzewski <erik.zak.sf@gmail.com>
2017-08-11 11:14:04 -0700

    add functionality to the subnit button and finsih the if else statement used

--- js/scripts.js
+++ js/scripts.js
@@ -3,18 +3,19 @@ $(document).ready(function(event) {
   $("form#track").submit(function(event) {
   event.preventDefault();
     var total = 0;
-    total += parseInt($("select#fave").val());
-    total += parseInt($("select#date").val());
+    total += parseInt($("select#webdev").val());
     total += parseInt($("select#choice").val());
+    total += parseInt($("select#date").val());
+    total += parseInt($("select#size").val());
+    total += parseInt($("select#hello").val());
 
-    if (total === 31){
-      $(".mixed").show();}
-    else if (total <= 12){
-      $(".action").show();}
-    else if (total < 41){
-      $(".comedy").show();}
+
+    if ((total === 5) || (total === 15)||(total === 24)||(total === 25)||(total === 34)||(total === 43) ){
+      $(".csharp").show();}
+    else if ((total === 35) || (total === 45)||(total === 55)||(total === 64)||(total === 73)||(total === 54) ){
+      $(".ruby").show();}
     else{
-      $(".romance").show();
+      $(".design").show();
 
     }
 
