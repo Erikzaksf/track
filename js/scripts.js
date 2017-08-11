8fcf8c3083b74f0d3d14787f251700060c471319
Erik Zakrzewski <erik.zak.sf@gmail.com>
2017-08-11 08:52:09 -0700

    add html, css and js pages from a previos project to use for template

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,25 @@
-lkfjaslkfjas
+
+$(document).ready(function(event) {
+  $("form#movie").submit(function(event) {
+  event.preventDefault();
+    var total = 0;
+    total += parseInt($("select#fave").val());
+    total += parseInt($("select#date").val());
+    total += parseInt($("select#choice").val());
+
+    if (total === 31){
+      $(".mixed").show();}
+    else if (total <= 12){
+      $(".action").show();}
+    else if (total < 41){
+      $(".comedy").show();}
+    else{
+      $(".romance").show();
+
+    }
+
+
+
+
+  });
+});
